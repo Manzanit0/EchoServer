eaa735b90e6bb8642d07a60c4798337e03565cec
Manzanit0 <unico.javier@gmail.com>
2018-11-15 13:48:28 +0000

    Fixes abstraction levels on Server class.

--- src/core/server/Server.java
+++ src/core/server/Server.java
@@ -9,34 +9,42 @@ import java.net.Socket;
 
 public class Server {
     private ServerSocket serverSocket;
-    private IOHandler out;
+    private IOHandler serverConsoleIO;
     private SocketIOHandler socketIO;
 
-    public Server(ServerSocket serverSocket, IOHandler out) {
+    public Server(ServerSocket serverSocket, IOHandler serverConsoleIO) {
         this.serverSocket = serverSocket;
-        this.out = out;
+        this.serverConsoleIO = serverConsoleIO;
     }
 
     public void start() throws IOException {
-        out.write("server is running on port: " + getPort());
+        publishMessage("server is running on port: " + getPort(), serverConsoleIO);
 
+        acceptIncomingConnection();
+        publishMessage("A new client has connected.", serverConsoleIO);
+
+        receiveClientMessages();
+    }
+
+    private void acceptIncomingConnection() throws IOException {
         Socket socket = serverSocket.accept();
         socketIO = new SocketIOHandler(socket);
-        publishNewClientConnection();
+    }
 
+    private void receiveClientMessages() throws IOException {
         String message;
         while((message = getClientMessage()) != null) {
-            out.write(message);
-            socketIO.write(message);
+            publishMessage(message, serverConsoleIO);
+            publishMessage(message, socketIO);
         }
+    }
+
+    private void publishMessage(String message, IOHandler out) {
+        out.write(message);
     }
 
     public int getPort() {
         return serverSocket.getLocalPort();
-    }
-
-    public void publishNewClientConnection() {
-        out.write("A new client has connected.");
     }
 
     public String getClientMessage() throws IOException {
